c389776f9787f794b478581c779f6e8679c62ccf
桂小方 <guiqiqi187@gmail.com>
2019-03-02 20:00:26 +0300

    Unittest

---
+++ test.py
@@ -0,0 +1,28 @@
+"""Unittest"""
+
+# from consts import *
+# __import__('pdb').set_trace()
+# from calculator import Calculate
+# from tools import Tools
+
+# string = \
+# """
+# 6.33 6.00 5.78 6.04 6.08
+# 6.23 5.94 5.96 6.16 5.90
+# 6.10 6.19 6.31 6.10 6.26
+# 5.98 5.88 5.94 6.08 6.21
+# 6.02 6.06 6.10 5.93 6.19
+# 5.85 5.50 5.56 6.52 6.03
+# 5.96 6.26 6.15 5.90 6.23
+# 6.10 5.83 5.43 6.36 6.06
+# 6.90 6.44 6.26 6.15 6.24
+# 6.20 6.40 6.42 6.39 6.43
+# """
+# standard = 6
+
+# datas = Tools.spliter(string)
+# instance = Calculate(standard, datas)
+
+# print (Tools.title())
+# print (Tools.table(instance.iterators()), end = '')
+# print (Tools.annotation(**instance.information()))
