7b12d491d5ab4d3a5f976c9747230f697869f0a7
桂小方 <guiqiqi187@gmail.com>
2019-03-02 19:59:07 +0300

    Const define

---
+++ consts.py
@@ -0,0 +1,21 @@
+"""Define consts"""
+
+import math
+import matplotlib.pyplot as plt
+
+from tkinter.filedialog import askopenfilename
+from tkinter import *
+import tkinter.messagebox as msgbox
+from tkinter.ttk import *
+
+import os
+
+ACCURACY = 10000 # default accuracy is 10**-5
+WIDTH = 12 # default width for every table column
+SEGMENT = 5 # default segments for operator linspace
+
+AXIS = "axis.png" # default axis figure filename
+BAR = "bar.png" # default bar figure filename
+TABLE = "table.txt" # default table filename
+
+HELP = "help" # Help file
